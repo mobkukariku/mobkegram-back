1aa58a71b27ad9d6b476916a195b0fd9b34f2157
Damir Tagankhozhaev <155202834+mobkukariku@users.noreply.github.com>
2025-01-19 18:08:07 +0500

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,8 +7,8 @@ export const generateToken = (userId:string, res:Response) => {
     });
     res.cookie("jwt", token, {
         httpOnly: true,
-        secure: false,
-        sameSite: 'none',
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         maxAge: 7 * 24 * 60 * 60 * 1000
     })
 
