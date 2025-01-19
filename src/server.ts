98827c2f3de5f206301b9d334948e524474c3e0a
Mobkus <tagankhozhaev@gmail.com>
2025-01-19 16:29:49 +0500

    fix:cors

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,7 @@ dotenv.config();
 const port = process.env.PORT;
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: '*',
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 };
