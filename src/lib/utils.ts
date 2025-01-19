25aea12357fc86d714e154680720c1d2f88b76f8
Mobkus <tagankhozhaev@gmail.com>
2025-01-19 18:01:18 +0500

    fix:cors

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,7 +8,7 @@ export const generateToken = (userId:string, res:Response) => {
     res.cookie("jwt", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV !== "development",
-        sameSite: "strict",
+        sameSite: process.env.NODE_ENV !== 'development' ? 'none' : 'strict',
         maxAge: 7 * 24 * 60 * 60 * 1000
     })
 
